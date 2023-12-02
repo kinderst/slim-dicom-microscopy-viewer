8cd296e2aa4bdec30adcfcbb385c282430091baa
Scott Kinder <kinders@uw.edu>
2023-12-02 13:46:54 -0700

    first version slim dicom microscopy viewer

---
+++ src/webWorker/decoders/decoderJPEG2000.js
@@ -0,0 +1,28 @@
+import openJpegFactory from '@cornerstonejs/codec-openjpeg/decodewasmjs'
+import openjpegWasm from '@cornerstonejs/codec-openjpeg/decodewasm'
+import Decoder from './decoderAbstract.js'
+
+export default class JPEG2000Decoder extends Decoder {
+  _initialize () {
+    if (this.codec) {
+      return Promise.resolve()
+    }
+
+    const openJpegModule = openJpegFactory({
+      locateFile: (f) => {
+        if (f.endsWith('.wasm')) {
+          return openjpegWasm
+        }
+        return f
+      }
+    })
+
+    return new Promise((resolve, reject) => {
+      openJpegModule.then((instance) => {
+        this.codec = instance
+        this.decoder = new instance.J2KDecoder()
+        resolve()
+      }, reject)
+    })
+  }
+}
